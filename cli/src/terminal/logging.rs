770c7f05fa312136a4dbe5ba3c8937a2bfc009f3
hollowpointer <hollowpointer@pm.me>
2026-01-21 00:22:54 +0100

    wrapped 'tracing' logging with own module, 'info' and 'success' are now printed separately

--- cli/src/terminal/logging.rs
+++ cli/src/terminal/logging.rs
@@ -14,7 +14,7 @@ where
 {
     fn format_event(
         &self,
-        ctx: &tracing_subscriber::fmt::FmtContext<'_, S, N>,
+        _ctx: &tracing_subscriber::fmt::FmtContext<'_, S, N>,
         mut writer: Writer<'_>,
         event: &Event<'_>,
     ) -> std::fmt::Result {
@@ -26,20 +26,68 @@ where
             return write!(writer, "\r\n");
         }
 
+        let mut status_visitor = StatusVisitor::default();
+        event.record(&mut status_visitor);
+
         let (symbol, color_func): (&str, fn(ColoredString) -> ColoredString) = match *meta.level() {
             Level::TRACE => ("[ ]", |s| s.dimmed()),
             Level::DEBUG => ("[?]", |s| s.blue()),
-            Level::INFO => ("[+]", |s| s.green().bold()),
+            Level::INFO => match status_visitor.status.as_deref() {
+                Some("info") => ("[»]", |s| s.cyan().bold()),
+                _ => ("[+]", |s| s.green().bold()),
+            },
             Level::WARN => ("[*]", |s| s.yellow().bold()),
             Level::ERROR => ("[-]", |s| s.red().bold()),
         };
 
         write!(writer, "{} ", color_func(symbol.into()))?;
-        ctx.field_format().format_fields(writer.by_ref(), event)?;
+
+        let mut output_visitor = OutputVisitor::new(writer.by_ref());
+        event.record(&mut output_visitor);
+
         write!(writer, "\r\n")
     }
 }
 
+// Visitors
+
+#[derive(Default)]
+struct StatusVisitor {
+    status: Option<String>,
+}
+
+impl Visit for StatusVisitor {
+    fn record_debug(&mut self, _field: &Field, _value: &dyn std::fmt::Debug) {}
+    fn record_str(&mut self, field: &Field, value: &str) {
+        if field.name() == "status" {
+            self.status = Some(value.to_string());
+        }
+    }
+}
+
+struct OutputVisitor<'a> {
+    writer: Writer<'a>,
+}
+
+impl<'a> OutputVisitor<'a> {
+    fn new(writer: Writer<'a>) -> Self {
+        Self { writer }
+    }
+}
+
+impl<'a> Visit for OutputVisitor<'a> {
+    fn record_debug(&mut self, field: &Field, value: &dyn std::fmt::Debug) {
+        if field.name() == "status" { return; }
+
+        if field.name() == "message" {
+            let _ = write!(self.writer, "{:?}", value);
+        } else {
+            let _ = write!(self.writer, " {}={:?}", field.name().italic(), value);
+        }
+    }
+}
+
+/// Handles raw printing
 struct RawVisitor<'a> {
     writer: Writer<'a>,
 }
