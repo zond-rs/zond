d938d63834ff03442f4abb7d0508ab6e562a6554
hollowpointer <hollowpointer@pm.me>
2026-07-18 23:55:55 +0200

    workspace cleanup for split

---
+++ target/debug/build/zond-plugins-769d94b6fd11e417/build_script_build-769d94b6fd11e417.d
@@ -0,0 +1,5 @@
+/Users/erik/GitHub/zond-cli/target/debug/build/zond-plugins-769d94b6fd11e417/build_script_build-769d94b6fd11e417.d: /Users/erik/GitHub/zond-engine/plugins/build.rs
+
+/Users/erik/GitHub/zond-cli/target/debug/build/zond-plugins-769d94b6fd11e417/build_script_build-769d94b6fd11e417: /Users/erik/GitHub/zond-engine/plugins/build.rs
+
+/Users/erik/GitHub/zond-engine/plugins/build.rs:
